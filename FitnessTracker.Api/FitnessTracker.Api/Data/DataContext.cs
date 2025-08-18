8fad995b8a1b866ef5a59ff086690b6bda541e22
Manish Singh <your-real-email@example.com>
2025-08-18 17:14:12 +0530

    feat: Add User model, DbContext, and initial migration

---
+++ FitnessTracker.Api/FitnessTracker.Api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using FitnessTracker.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTracker.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
